eb52b5ccdf16f01533bd747758c8204bd33aaba1
Kos <koshuang@gmail.com>
2015-10-02 17:22:58 +0800

    add first spec for predict

--- test/predict.spec.js
+++ test/predict.spec.js
@@ -1,9 +1,27 @@
 'use strict';
 
 var fs = require('fs');
+var Predict = require('../src/predict');
+var predict;
 
 describe('Predict', function() {
+  beforeEach(function(done) {
+
+    fs.readFile('./string.txt', function (error, data) {
+      predict = new Predict(data);
+
+      done();
+    });
+  });
+
   it('should work', function() {
-    expect(true).toBe(true);
+    var sequence = '27';
+
+    var exactWords = predict.findWords(sequence, predict.tree, true);
+
+    expect(exactWords.length).toBe(3);
+    expect(exactWords[0].word).toBe('ar');
+    expect(exactWords[1].word).toBe('arx');
+    expect(exactWords[2].word).toBe('ars');
   });
 });
